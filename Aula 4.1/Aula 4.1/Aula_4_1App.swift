a6e4784c749f934ddac561418114d97b6e056f02
Gabriel de Albuquerque <128191671+gabrieldealbuquerque@users.noreply.github.com>
2024-06-11 21:42:53 -0300

    Add files via upload

---
+++ Aula 4.1/Aula 4.1/Aula_4_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Aula_4_1App.swift
+//  Aula 4.1
+//
+//  Created by Turma02-9 on 11/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Aula_4_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Aula 4.1/Modo1.swift
@@ -0,0 +1,27 @@
+//
+//  Modo 1.swift
+//  Aula 4.1
+//
+//  Created by Turma02-9 on 11/06/24.
+//
+
+import SwiftUI
+
+struct Modo1: View {
+    var body: some View {
+        ZStack
+        {
+            Color(.blue).ignoresSafeArea()
+            Rectangle().frame(width: 250, height: 150).cornerRadius(10).foregroundColor(.pink)
+            VStack
+            {
+                Text("Nome: Tod").foregroundStyle(.white)
+                Text("Sobrenome: Santos").foregroundStyle(.white)
+            }
+        }
+    }
+}
+
+#Preview {
+    Modo1()
+}
